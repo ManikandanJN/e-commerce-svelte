be59e268d97ac73f499bf20c4b947c3e39d095a2
Manikandan610 <manikandan.jn@fibonalabs.com>
2024-10-12 11:56:56 +0530

    e-commerce svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Dashboard from '../components/dashboard/dashboard.svelte';
+  import Sidebar from '../components/sidebar/sidebar.svelte';
+</script>
+
+<div class="main-page">
+  <Sidebar />
+  <Dashboard />
+  <!-- <h1 class="text-3xl font-bold underline">Hello world</h1> -->
+</div>
+
+<style>
+  .main-page{
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+  }
+</style>
